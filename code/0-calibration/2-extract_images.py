5a25c2d313c388899e84918f1e82a9e487c6aed0
wgrosche <wilkegrosche@gmail.com>
2024-07-03 18:11:19 +0200

    some hloc experiments and posing pipeline

--- code/0-calibration/2-extract_images.py
+++ code/0-calibration/2-extract_images.py
@@ -20,7 +20,8 @@ Calculates the extrinsics for all cameras with environment footage
 
 force_reconstruction = False
 num_extract = 2
-num_extract_omni = 40
+num_extract_omni = 20
+interval_omni = 60
 skip_percent_frames = 0.10
 
 # imports
@@ -32,6 +33,8 @@ import subprocess
 import pyexif
 from pathlib import Path
 import py360convert
+from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 # Path to the code directory
 BASEPATH = os.path.dirname(os.path.abspath(__file__)).split('code')[-2]
@@ -47,6 +50,8 @@ from utils.io_utils import write_json
 
 omni_frame_ids = np.linspace(1200, 2700, 30, dtype=int)
 persp_frame_ids = [100]
+omni_frame_min = 1200
+omni_frame_max = 2700
 
 
 try:
@@ -64,96 +69,121 @@ opensfm_repo = Path(config.get('calibration',
 
 # set up paths
 env_footage = data_root / 'raw_data' / 'footage'
-opensfm_data =  data_root / '0-calibration' / 'opensfm'
-opensfm_images = opensfm_data / 'images'
-extrinsics_path = data_root / '0-calibration' / 'extrinsics'
+# opensfm_data =  data_root / '0-calibration' / 'opensfm'
+images_dir = data_root / '0-calibration' / 'images'
+output_dir = data_root / '0-calibration' / 'outputs'
+
+REEXTRACT = True
+
+def process_omni_frame(frame, camera:Camera, index, face_w):
+    perspectives = py360convert.e2c(frame, face_w=face_w, cube_format='list')
+    for j, image in enumerate(perspectives):
+        file_path =  images_dir / f'{camera.name}_{index}_{j}.jpg' 
+        cv2.imwrite(str(file_path), image)
+        metadata = pyexif.ExifEditor(file_path)
+        metadata.setTag('Model', camera.name)
+        metadata.setTag('Make', camera.name)
 
 def main():
-    if ((opensfm_data / 'reconstruction.json').is_file()
-        and not force_reconstruction):
-        log.info("Reconstruction already exists. Skipping reconstruction.")
+    log.info("Extracting Images...")
     
-    else:
-        log.info("Running SfM pipeline...")
-        # Check if openSfM repository exists
-        if not opensfm_repo.is_dir():
-            raise ValueError(
-                "Cannot run openSFM since it has not been "
-                "cloned yet, follow instructions at:"
-                "https://opensfm.readthedocs.io/en/latest/building.html")
+    
+
+    if REEXTRACT:
+        if images_dir.is_dir():
+            log.info("Removing existing images directory.")
+            subprocess.run(f"rm -r {images_dir}", shell=True)
+
+    images_dir.mkdir(parents=True, exist_ok=True)
+
+    cams = get_cam_names(env_footage)
+    camera_models_overrides_dict = {}
+    log.info(f"Found {(cams)} in environment footage.")
+
+    for cam in cams:
+        camera = Camera(cam, newest=False)
         
-        opensfm_images.mkdir(parents=True, exist_ok=True)
+        first_frame = int(skip_percent_frames*(camera.num_frames))
+        last_frame = camera.num_frames - first_frame
 
-        cams = get_cam_names(env_footage)
-        camera_models_overrides_dict = {}
-        log.info(f"Found {(cams)} in environment footage.")
-
-        for cam in cams:
-            camera = Camera(cam, newest=False)
-            
-            first_frame = int(skip_percent_frames*(camera.num_frames))
-            last_frame = camera.num_frames - first_frame
-
-            cam_dict = {}
-            if camera.is_omni:
-                frame_ids = np.linspace(first_frame, last_frame, 
+        cam_dict = {}
+        if camera.is_omni:
+            # num_extract_omni = (last_frame - first_frame) // 60
+            frame_ids = np.linspace(first_frame, last_frame, 
+                                    num_extract_omni, dtype=int)
+            if omni_frame_ids is not None:
+                frame_ids = omni_frame_ids
+            elif omni_frame_min is not None and omni_frame_max is not None:
+                frame_ids = np.linspace(omni_frame_min, omni_frame_max, 
                                         num_extract_omni, dtype=int)
-                if omni_frame_ids is not None:
-                    frame_ids = omni_frame_ids
-                frames = camera.extract(frame_ids)
-                height, width = frames[0].shape[:-1]
-                proj_type = 'perspective'
-                cam_dict["projection_type"] = proj_type
-                face_w = 512
-                cam_dict["focal"] = face_w / 2
-                cam_dict["width"] = face_w
-                cam_dict["height"] = face_w
-                
-                for i, frame in enumerate(frames):
-                    perspectives = py360convert.e2c(frames[i], face_w=face_w, cube_format='list')
-                    for j, image in enumerate(perspectives):
-                        index = frame_ids[i]
-                        file_path =  opensfm_images / f'{camera.name}_{index}_{j}.jpg' 
-                        cv2.imwrite(str(file_path), image)
-                        metadata = pyexif.ExifEditor(file_path)
-                        metadata.setTag('Model', camera.name)
-                        metadata.setTag('Make', camera.name)
-                        cam_key = f"v2 {cam}_side_{j}  {face_w} {face_w} perspective 0.0"
-                camera_models_overrides_dict[cam_key] = cam_dict
+            all_frame_ids = frame_ids
+            batch_size = 10
+            with ThreadPoolExecutor() as executor:
+                futures = []
+                for batch_start in tqdm(range(0, len(all_frame_ids), batch_size), desc=f"Extracting {cam} frames"):
+                    batch_frame_ids = all_frame_ids[batch_start:batch_start+batch_size]
+                    frames = camera.extract(batch_frame_ids)
+                    height, width = frames[0].shape[:-1]
+                    proj_type = 'perspective'
+                    cam_dict["projection_type"] = proj_type
+                    face_w = 512
+                    cam_dict["focal"] = face_w / 2
+                    cam_dict["width"] = face_w
+                    cam_dict["height"] = face_w
+                    for i, frame in enumerate(frames):
+                        index = batch_frame_ids[i]
+                        futures.append(executor.submit(process_omni_frame, frame, camera, index, face_w))
 
-            else:
-                frame_ids = np.linspace(first_frame, last_frame, 
-                                        num_extract, dtype=int)
-                if persp_frame_ids is not None:
-                    frame_ids = persp_frame_ids
-                frames = camera.extract(frame_ids)
-                K = camera.get_calib().K
-                frames = camera.undistort(frames=frames)
-                height, width = frames[0].shape[:-1]
-                focal_length = (K[0][0]+K[1][1]) / (np.max([height, width])*2)
-                proj_type ='perspective'
-                cam_dict["projection_type"] = proj_type
-                cam_dict["focal"] = focal_length
-
-                for i, frame in enumerate(frames):
-                    index = frame_ids[i]
-                    file_path =  opensfm_images / f'{camera.name}_{index}.jpg'              
-                    cv2.imwrite(str(file_path), frame)
-                    metadata = pyexif.ExifEditor(file_path)
-                    metadata.setTag('Model', camera.name)
-                    metadata.setTag('Make', camera.name)
-
-                cam_dict["width"] = width
-                cam_dict["height"] = height
-                cam_key = f"v2 {cam}  {width} {height} perspective 0.0"
-                camera_models_overrides_dict[cam_key] = cam_dict
+                for future in as_completed(futures):
+                    pass
+                            
 
 
-        # Write to file
-        log.info("Adding overrides to opensfm data directory...")
-        overrides_filepath = opensfm_data / "camera_models_overrides.json"
+                # for index in tqdm(frame_ids, desc=f"Extracting {cam} frames"):
+                #     frame = camera.extract([index])[0]
+                #     height, width = frame.shape[:-1]
+                #     proj_type = 'perspective'
+                #     cam_dict["projection_type"] = proj_type
+                #     face_w = 512
+                #     cam_dict["focal"] = face_w / 2
+                #     cam_dict["width"] = face_w
+                #     cam_dict["height"] = face_w
+                #     perspectives = py360convert.e2c(frame, face_w=face_w, cube_format='list')
+                #     for j, image in enumerate(perspectives):
+                #         file_path =  images_dir / f'{camera.name}_{index}_{j}.jpg' 
+                #         cv2.imwrite(str(file_path), image)
+                #         metadata = pyexif.ExifEditor(file_path)
+                #         metadata.setTag('Model', camera.name)
+                #         metadata.setTag('Make', camera.name)
+                #         cam_key = f"v2 {cam}_side_{j}  {face_w} {face_w} perspective 0.0"
+                # camera_models_overrides_dict[cam_key] = cam_dict
 
-        write_json(overrides_filepath, camera_models_overrides_dict)
+        else:
+            frame_ids = np.linspace(first_frame, last_frame, 
+                                    num_extract, dtype=int)
+            if persp_frame_ids is not None:
+                frame_ids = persp_frame_ids
+            frames = camera.extract(frame_ids)
+            K = camera.get_calib().K
+            frames = camera.undistort(frames=frames)
+            height, width = frames[0].shape[:-1]
+            focal_length = (K[0][0]+K[1][1]) / (np.max([height, width])*2)
+            proj_type ='perspective'
+            cam_dict["projection_type"] = proj_type
+            cam_dict["focal"] = focal_length
+
+            for i, frame in tqdm(enumerate(frames), desc=f"Extracting {cam} frames"):
+                index = frame_ids[i]
+                file_path =  images_dir / f'{camera.name}_{index}.jpg'              
+                cv2.imwrite(str(file_path), frame)
+                metadata = pyexif.ExifEditor(file_path)
+                metadata.setTag('Model', camera.name)
+                metadata.setTag('Make', camera.name)
+
+            cam_dict["width"] = width
+            cam_dict["height"] = height
+            cam_key = f"v2 {cam}  {width} {height} perspective 0.0"
+            camera_models_overrides_dict[cam_key] = cam_dict
 
 if __name__ == "__main__":
     main()
